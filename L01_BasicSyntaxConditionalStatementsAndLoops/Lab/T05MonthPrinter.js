bc291a538e7e87a6f84641f5740d60fa1ee4f657
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-27 11:51:29 +0200

    Add files via upload

---
+++ L01_BasicSyntaxConditionalStatementsAndLoops/Lab/T05MonthPrinter.js
@@ -0,0 +1,23 @@
+function solve(number) {
+    const months = ['January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'];
+
+    if (number >= 1 && number <= 12) {
+        console.log(months[number - 1]);
+    } else {
+        console.log('Error!');
+    }
+}
+
+solve(2);
+solve(13);
