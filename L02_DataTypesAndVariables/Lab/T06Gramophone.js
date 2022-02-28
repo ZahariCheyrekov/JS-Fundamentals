4de9f4082b0fc9e803031ddd900797809265280b
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-28 10:09:30 +0200

    Add files via upload

---
+++ L02_DataTypesAndVariables/Lab/T06Gramophone.js
@@ -0,0 +1,9 @@
+function solve(band, album, song) {
+    let duration = (album.length * band.length) * song.length / 2;
+    let rotations = Math.ceil(duration / 2.5);
+
+    console.log(`The plate was rotated ${rotations} times.`);
+}
+
+solve('Black Sabbath', 'Paranoid', 'War Pigs');
+solve('Rammstein', 'Sehnsucht', 'Engel');
