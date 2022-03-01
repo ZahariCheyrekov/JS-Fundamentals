cef3ed8aa2bac5eda09926298a09b82b55023855
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-01 13:00:59 +0200

    Add files via upload

---
+++ L03_Arrays/Lab/T06EvenAndOddSubstraction.js
@@ -0,0 +1,14 @@
+function solve(arr) {
+    let evenSum = 0;
+    let oddSum = 0;
+
+    arr.forEach(num => {
+        num % 2 === 0 ? evenSum += num : oddSum += num;
+    });
+
+    console.log(evenSum - oddSum);
+}
+
+solve([1, 2, 3, 4, 5, 6]);
+solve([3, 5, 7, 9]);
+solve([2, 4, 6, 8, 10]);
