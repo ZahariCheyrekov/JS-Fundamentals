2e82f74ebd4b769278a1f282745e0d9a268296d0
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-01 13:02:24 +0200

    Add files via upload

---
+++ L03_Arrays/Exercises/T04ArrayRotation.js
@@ -0,0 +1,13 @@
+function solve(arr, rotations) {
+
+    for (let i = 0; i < rotations; i++) {
+        const item = arr.shift();
+        arr.push(item);
+    }
+
+    console.log(arr.join(' '));
+}
+
+solve([51, 47, 32, 61, 21], 2);
+solve([32, 21, 61, 1], 4);
+solve([2, 4, 15, 31], 5);
