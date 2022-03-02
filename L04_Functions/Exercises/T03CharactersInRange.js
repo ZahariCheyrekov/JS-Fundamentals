e8ae3e4a8d1fa2fe170a77627a92c390f84cb075
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-02 12:56:49 +0200

    Add files via upload

---
+++ L04_Functions/Exercises/T03CharactersInRange.js
@@ -0,0 +1,20 @@
+function solve(...args) {
+    let result = [];
+
+    const start = args[0].charCodeAt(0);
+    const end = args[1].charCodeAt(0);
+
+    function display(start, end) {
+        for (let i = start + 1; i < end; i++) {
+            result.push(String.fromCharCode(i));
+        }
+    }
+
+    start < end ? display(start, end) : display(end, start);
+
+    console.log(result.join(' '))
+}
+
+solve('a', 'd');
+solve('#', ':');
+solve('C', '#');
