3c9ff9e5cb7936b5668ade9a3c7e5c6dfcb87521
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-03 11:47:42 +0200

    Add files via upload

---
+++ L05_ArraysAdvanced/Lab/T06SmallestTwoNumbers.js
@@ -0,0 +1,10 @@
+function solve(arr) {
+    const result = arr
+        .sort((a, b) => a - b)
+        .slice(0, 2);
+
+    console.log(result.join(' '));
+}
+
+solve([30, 15, 50, 5]);
+solve([3, 0, 10, 4, 7, 3]);
