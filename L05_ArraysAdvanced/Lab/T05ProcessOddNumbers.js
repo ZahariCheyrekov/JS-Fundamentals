c99328e8b8266a67c5eede8ff4e5df314c7803ac
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-03 11:47:14 +0200

    Add files via upload

---
+++ L05_ArraysAdvanced/Lab/T05ProcessOddNumbers.js
@@ -0,0 +1,11 @@
+function solve(arr) {
+    const result = arr
+        .filter((_, i) => i % 2 == 1)
+        .map(x => x * 2)
+        .reverse();
+        
+    console.log(result.join(' '));
+}
+
+solve([10, 15, 20, 25]);
+solve([3, 0, 10, 4, 7, 3]);
