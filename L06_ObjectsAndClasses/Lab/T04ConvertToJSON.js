9fd124fdb666c349cf297c216cc3c70d15873d2d
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-04 09:55:47 +0200

    Add files via upload

---
+++ L06_ObjectsAndClasses/Lab/T04ConvertToJSON.js
@@ -0,0 +1,13 @@
+function solve(firstName, lastName, hairColor) {
+
+    let person = {
+        'name': firstName,
+        lastName,
+        hairColor
+    };
+
+    console.log(JSON.stringify(person));
+}
+
+solve('George', 'Jones', 'Brown');
+solve('Peter', 'Smith', 'Blond');
