0e689e16191635dbd6526e2d86c3fd5771ff7bda
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-06 10:30:56 +0200

    Add files via upload

---
+++ L08_TextProcessing/Exercises/T04StringSubstring.js
@@ -0,0 +1,9 @@
+function solve(str1, str2) {
+    const sentence = str2.split(' ').map(x => x.toLocaleLowerCase());
+    
+    if (sentence.includes(str1.toLocaleLowerCase())) {
+        return str1;
+    }
+
+    return `${str1} not found!`;
+}
