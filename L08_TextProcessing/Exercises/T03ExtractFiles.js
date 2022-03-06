bd4655f136f2f0d87838bdefcba1a3ce44ba47fe
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-03-06 10:30:41 +0200

    Add files via upload

---
+++ L08_TextProcessing/Exercises/T03ExtractFiles.js
@@ -0,0 +1,9 @@
+function solve(str) {
+
+    const match = str.substring(str.lastIndexOf('\\') + 1);
+    const name = match.substring(0, match.lastIndexOf('.'));
+    const extension = match.substring(match.lastIndexOf('.') + 1);
+
+    console.log(`File name: ${name}
+File extension: ${extension}`);
+}
